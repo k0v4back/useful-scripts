84d16235d2801c3c64fe522cb042f4c3cddd12ff
k0v4 <vadkos33@outlook.com>
2022-02-27 13:32:18 +0300

    Bash script for copy file or files to boot partition on SD card

---
+++ bash/copy_file_to_boot_partition.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo -n "Enter the name of the copied file (am335x-boneblack.dtb or another): "
+read FILE_NAME
+
+umount /tmp
+mount /dev/mmcblk0p1 /tmp
+cp FILE_NAME /tmp
+sync
+umount /tmp
+echo 4 > /proc/sys/kernel/printk
+
+# Check errors
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo "Mistake"
+fi
